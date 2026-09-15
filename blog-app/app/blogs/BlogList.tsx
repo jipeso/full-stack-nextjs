4eb2019f6254e51818b3b797e2ffcc563fbd3be0
jipeso <149601375+jipeso@users.noreply.github.com>
2026-09-15 17:49:52 +0300

    exercise 5

--- blog-app/app/blogs/BlogList.tsx
+++ blog-app/app/blogs/BlogList.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link'
 import type { Blog } from '../types'
 
 const BlogList = ({ blogs }: { blogs: Blog[] }) => {
+  const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
+
   return (
     <div>
       <ul>
-        {blogs.map(blog => (
+        {sortedBlogs.map(blog => (
           <li key={blog.id}>
             <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
             {blog.author} {blog.url} {blog.likes}
